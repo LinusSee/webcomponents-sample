8121149bcfbb2b35192907baa2c2e3bcd91b726f
Linus <linussee@web.de>
2020-10-06 20:36:43 +0200

    feat: add input and event listener to web component

--- main-app/src/app/app.component.ts
+++ main-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { UpdateUserModel } from './models/update-user.model';
+import { UserModel } from './models/user.model';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'main-app';
+  public user: UserModel;
+
+  public ngOnInit(): void {
+     this.user = {
+        username: 'user1337',
+        email: 'user1337@gmail.com',
+        city: 'Berlin',
+        street: 'Hauptstraße',
+        streetNumber: '12',
+        messages: [
+           'Please check your spam folder for a new IPhone',
+           'The weather is quite nice, isn\'t it?',
+        ],
+     };
+  }
+
+  public updateUser(event: UpdateUserModel): void {
+     console.log('Event: ', event);
+     this.user.username = event.username;
+     this.user.email = event.email;
+  }
 }
