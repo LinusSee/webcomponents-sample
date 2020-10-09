7e4ddadbac48b8abfaab74b732795f6352b27a7e
Linus <linussee@web.de>
2020-10-09 21:11:50 +0200

    feat: dummy components and routing

--- main-app/src/app/app-routing.module.ts
+++ main-app/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LogfileAnalysisComponent } from './logfile-analysis/logfile-analysis.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const routes: Routes = [
+   {
+      path: 'user-profile',
+      component: UserProfileComponent,
+   },
+   {
+      path: 'logfile-analysis',
+      component: LogfileAnalysisComponent,
+   },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- main-app/src/app/app.module.ts
+++ main-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LogfileAnalysisComponent } from './logfile-analysis/logfile-analysis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserProfileComponent,
+    LogfileAnalysisComponent,
   ],
   imports: [
     BrowserModule,
