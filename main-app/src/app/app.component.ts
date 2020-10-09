f8a946682c928216a3df9bb959648d22d5e66a1b
Linus <linussee@web.de>
2020-10-09 22:28:40 +0200

    fix: move logic to web component

--- main-app/src/app/app.component.ts
+++ main-app/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { UpdateUserModel } from './models/update-user.model';
-import { UserModel } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +6,5 @@ import { UserModel } from './models/user.model';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  public user: UserModel;
 
-  public ngOnInit(): void {
-     this.user = {
-        username: 'user1337',
-        email: 'user1337@gmail.com',
-        city: 'Berlin',
-        street: 'Hauptstraße',
-        streetNumber: '12',
-        messages: [
-           'Please check your spam folder for a new IPhone',
-           'The weather is quite nice, isn\'t it?',
-        ],
-     };
-  }
-
-  public updateUser(event: UpdateUserModel): void {
-     console.log('Event: ', event);
-     this.user.username = event.username;
-     this.user.email = event.email;
-  }
 }
--- main-app/src/app/user-profile/user-profile.component.ts
+++ main-app/src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UpdateUserModel } from './../models/update-user.model';
+import { UserModel } from './../models/user.model';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+   public user: UserModel;
 
-  ngOnInit(): void {
-  }
+   public ngOnInit(): void {
+      this.user = {
+         username: 'user1337',
+         email: 'user1337@gmail.com',
+         city: 'Berlin',
+         street: 'Hauptstraße',
+         streetNumber: '12',
+         messages: [
+            'Please check your spam folder for a new IPhone',
+            'The weather is quite nice, isn\'t it?',
+         ],
+      };
+   }
 
+   public updateUser(event: UpdateUserModel): void {
+      console.log('Event: ', event);
+      this.user.username = event.username;
+      this.user.email = event.email;
+   }
 }
