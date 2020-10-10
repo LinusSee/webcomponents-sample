37b34eda83de785d5b22b954eab3e3e9a733bd2c
Linus <linussee@web.de>
2020-10-10 11:09:36 +0200

    feat: dynamically include logfile web component

--- main-app/src/app/app.module.ts
+++ main-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+
 import { LogfileAnalysisComponent } from './logfile-analysis/logfile-analysis.component';
 
 @NgModule({
@@ -15,6 +18,7 @@ import { LogfileAnalysisComponent } from './logfile-analysis/logfile-analysis.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- main-app/src/app/logfile-analysis/logfile-analysis.component.ts
+++ main-app/src/app/logfile-analysis/logfile-analysis.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-logfile-analysis',
   templateUrl: './logfile-analysis.component.html',
-  styleUrls: ['./logfile-analysis.component.css']
+  styleUrls: ['./logfile-analysis.component.css'],
 })
 export class LogfileAnalysisComponent implements OnInit {
 
-  constructor() { }
+  public finishedLoadingScript = false;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit(): void {
+    this.loadScript().pipe(
+      tap((script: HTMLScriptElement) => document.body.appendChild(script)),
+      tap(( _ ) => this.finishedLoadingScript = true),
+    ).subscribe();
   }
 
+  private loadScript(): Observable<HTMLScriptElement> {
+    return this.http.get('http://localhost:3000/logfile-analysis/main.js', { responseType: 'blob' }).pipe(
+      map((blob: Blob) => this.createScriptElement(blob)),
+    );
+  }
+
+  private createScriptElement(blob: Blob): HTMLScriptElement {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = URL.createObjectURL(blob);
+    scriptElement.setAttribute('custom-name', 'someName');
+    return scriptElement;
+  }
 }
